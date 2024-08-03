58e75f104443cd54e292c699b2ff91a839763219
njagua <njagua.gma@gmail.com>
2024-08-03 19:05:00 +0300

    add

---
+++ src/components/Article.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    const emoji = minutes < 30 
+    ? "☕️".repeat(Math.ceil(minutes / 5)) 
+    : "🍱".repeat(Math.ceil(minutes / 10));
+
+    return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date} - {emoji}{minutes} minute read.</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
